dcd36e7022f71708096e897a4c9a7b3f73c38746
Emanuel Nader <117801468+EmanuelNader@users.noreply.github.com>
2026-08-18 23:19:41 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++){
+            int[] freq =  new int [26];
+            for (char c : strs[i].toCharArray()){
+                freq[c - 'a']++;
+            }
+            String key = Arrays.toString(freq);
+            if (!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(strs[i]);
+            }
+        return new ArrayList<>(map.values());
+        }
+}
+
